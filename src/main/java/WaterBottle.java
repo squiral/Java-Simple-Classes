a4fc8343631ffd8681838eaef6b8f459d7fae9cf
Alison Williams <alison.lauriewill@gmail.com>
2019-06-10 16:08:32 +0100

    MVP completed

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -13,4 +13,12 @@ public class WaterBottle {
     public void drink(){
         this.volume -= 10;
     }
+
+    public void empty(){
+        this.volume = 0;
+    }
+
+    public void fill(){
+        this.volume = 100;
+    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -22,4 +22,17 @@ public class WaterBottleTest {
         waterBottle.drink();
         assertEquals(90, waterBottle.getVolume());
     }
+
+    @Test
+    public void canEmpty(){
+        waterBottle.empty();
+        assertEquals(0, waterBottle.getVolume());
+    }
+
+    @Test
+    public void canFill(){
+        waterBottle.empty();
+        waterBottle.fill();
+        assertEquals(100, waterBottle.getVolume());
+    }
 }
