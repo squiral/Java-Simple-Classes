01da41522131479eeabdad02c55a1be2ab8ff778
Alison Williams <alison.lauriewill@gmail.com>
2019-06-10 19:21:41 +0100

    Extension Completed

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,16 +1,29 @@
 public class Printer {
 
     private int numberOfSheetsLeft;
+    private int toner;
 
-    public Printer(int numberOfSheetsLeft){
+    public Printer(int numberOfSheetsLeft, int toner){
         this.numberOfSheetsLeft = numberOfSheetsLeft;
+        this.toner = toner;
     }
 
     public int getNumberOfSheetsLeft(){
         return this.numberOfSheetsLeft;
     }
 
-    public void print(){
-
+    public int getToner(){
+        return this.toner;
     }
+
+    public void print(int numberOfCopies, int numberOfPages){
+        int totalPagesUsed = numberOfCopies * numberOfPages;
+        if (totalPagesUsed <= this.numberOfSheetsLeft && totalPagesUsed <= this.toner){
+            this.numberOfSheetsLeft -= totalPagesUsed;
+            this.toner -= totalPagesUsed;
+        } else {
+            return;
+        }
+    }
+
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -9,11 +9,42 @@ public class PrinterTest {
 
     @Before
     public void before(){
-        printer = new Printer(30);
+        printer = new Printer(30, 100);
     }
 
     @Test
     public void hasNumberOfSheetsLeft(){
         assertEquals(30, printer.getNumberOfSheetsLeft());
+    }
+
+    @Test
+    public void printMethodEnoughPages(){
+        printer.print(2, 3);
+        assertEquals(24, printer.getNumberOfSheetsLeft());
+    }
+
+    @Test
+    public void printMethodNotEnoughPages(){
+        printer.print(12, 4);
+        assertEquals(30, printer.getNumberOfSheetsLeft());
+    }
+
+    @Test
+    public void hasToner(){
+        assertEquals(100, printer.getToner());
+    }
+
+    @Test
+    public void tonerReducesWithEachPage(){
+        printer.print(2,3);
+        assertEquals(94, printer.getToner());
+
+    }
+
+    @Test
+    public void printMethodEnoughPaperNotEnoughToner(){
+        Printer lowTonerPrinter = new Printer(100,10);
+        printer.print(30,2);
+        assertEquals(100, lowTonerPrinter.getNumberOfSheetsLeft());
     }
 }
