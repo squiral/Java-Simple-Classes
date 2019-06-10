e0f7bf86924f727b209495c4969acfae5ed25d68
Alison Williams <alison.lauriewill@gmail.com>
2019-06-10 16:02:33 +0100

    started WaterBottle

---
+++ src/main/java/WaterBottle.java
@@ -0,0 +1,16 @@
+public class WaterBottle {
+
+    private int volume;
+
+    public WaterBottle(){
+        this.volume = 100;
+    }
+
+    public int getVolume(){
+        return this.volume;
+    }
+
+//    public void drink(){
+//        this.volume - 10;
+//    }
+}
---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class WaterBottleTest {
+
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle();
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, waterBottle.getVolume());
+    }
+
+//    @Test
+//    public void canDrink(){
+//        assertEquals(90,)
+//    }
+}
